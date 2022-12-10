a0888c998444157c041d9eff1a00a20a8729dd8f
santiagocadavid <santycadavid@hotmail.com>
2022-12-09 21:50:22 -0500

    Update Actividad.vue

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -15,7 +15,7 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/completar.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo.color-primario
           h2 Completar espacios
